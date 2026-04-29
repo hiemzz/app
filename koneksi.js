a7b3b9e2d916eac243e0525ee1242f964a0cad21
hiemzz <125773970+hiemzz@users.noreply.github.com>
2026-04-29 19:28:56 +0700

    Update koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -1,5 +1,5 @@
-// Konfigurasi Supabase - Ganti dengan data dari Project Settings > API di Supabase
-const supabaseUrl = 'https://odlvozfueejngirfrczo.supabase.co/rest/v1/';
+// koneksi.js
+const supabaseUrl = 'https://odlvozfueejngirfrczo.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbHZvemZ1ZWVqbmdpcmZyY3pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODU1MDYsImV4cCI6MjA5Mjk2MTUwNn0.yp03bnfws8IrMu_evwesLM8u0cIMIje8ufUhrFSsM38';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
