4e787938681bbb54db1206d466de076495d70092
hiemzz <125773970+hiemzz@users.noreply.github.com>
2026-04-29 20:09:55 +0700

    Update koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -1,6 +1,6 @@
-// Ganti isi koneksi.js kamu dengan kode ini sepenuhnya
-const supabaseUrl = 'https://odlvozfueejngirfrczo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbHZvemZ1ZWVqbmdpcmZyY3pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODU1MDYsImV4cCI6MjA5Mjk2MTUwNn0.yp03bnfws8IrMu_evwesLM8u0cIMIje8ufUhrFSsM38';
+// koneksi.js
+var supabaseUrl = 'https://odlvozfueejngirfrczo.supabase.co';
+var supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kbHZvemZ1ZWVqbmdpcmZyY3pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODU1MDYsImV4cCI6MjA5Mjk2MTUwNn0.yp03bnfws8IrMu_evwesLM8u0cIMIje8ufUhrFSsM38';
 
-// PERBAIKAN: Menimpa library bawaan agar langsung menjadi jembatan koneksi
-window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+// Gunakan var agar variabel ini terbaca di semua halaman
+var dbCloud = supabase.createClient(supabaseUrl, supabaseKey);
